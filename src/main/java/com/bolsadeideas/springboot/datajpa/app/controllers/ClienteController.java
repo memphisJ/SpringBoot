42821aa86d6dc21584005f0903b07e73efde5985
memphis <h.menphisxp@gmail.com>
2019-04-05 00:23:35 -0400

    El modulo de registro de adicion fue creado

--- src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteController.java
@@ -1,9 +1,16 @@
 package com.bolsadeideas.springboot.datajpa.app.controllers;
 
 import com.bolsadeideas.springboot.datajpa.app.model.dao.IClienteDao;
+import com.bolsadeideas.springboot.datajpa.app.model.entity.Cliente;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,4 +30,34 @@ public class ClienteController {
         return "listar";
     }
 
+	/**
+	 * Este metodo crear el formulario para que se ingresen los datos del cliente
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/form")
+	public String crear(Map<String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("cliente",cliente);
+		model.put("Titulo", "Formulario de Cliente");
+		return "form";
+	}
+	
+	/**
+	 * Este metodo recive los datos de la ventana de registro de cliente 
+	 * y lo envia al metodo de la DAO para hacer el respectivo regsitro 
+	 * en la BD.
+	 * @param cliente
+	 * @return
+	 */
+	@RequestMapping(value="/form", method=RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		model.addAttribute("Titulo", "Formulario de Cliente");
+		if(result.hasErrors()) {
+			return "form";
+		}
+		
+		clienteDao.save(cliente);
+		return "redirect:listar";
+	}
 }
